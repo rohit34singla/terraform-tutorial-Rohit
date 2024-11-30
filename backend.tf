a71c8f374ad01fbbea89b609f216d6afda1f9c92
Rohit34Singla <Rohit34singla@gmail.com>
2024-11-30 18:11:48 -0500

    deleted files for old tutorials

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "my-athena-bucket-2024"
-    key    = "terraform/state.tfstate"
-    region = "ca-central-1"
-    # dynamodb_table = "terraform-locks"
-    # encrypt        = true
-  }
-}
--- my_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Hello from Terraform Provisioner!" > /home/ubuntu/provisioner_output.txt
-sudo apt-get update -y
-sudo apt-get install -y nginx
