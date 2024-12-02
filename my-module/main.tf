f7dfad7f5e009d4aa8ddba379f183ff7aef29894
Rohit34Singla <Rohit34singla@gmail.com>
2024-12-02 11:00:14 -0500

    add lifecycle policy in the main.tf

--- my-module/main.tf
+++ my-module/main.tf
@@ -1,11 +1,15 @@
 resource "aws_instance" "example" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  count                  = var.instance_count
-  key_name               = var.key_name  # Associate the existing key pair
-  security_groups        = [var.security_group]  # Use the existing security group
+  ami             = var.ami
+  instance_type   = var.instance_type
+  count           = var.instance_count
+  key_name        = var.key_name         # Associate the existing key pair
+  security_groups = [var.security_group] # Use the existing security group
 
   tags = {
     Name = "${var.instance_name}-${count.index + 1}"
+  }
+  lifecycle {
+   # prevent_destroy       = true
+    create_before_destroy = true
   }
 }
